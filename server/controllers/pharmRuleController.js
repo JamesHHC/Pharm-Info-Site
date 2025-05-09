3cea32e35bc4a357d514cdd54ab3a8e55e75ebbe
Jamesrkiv <73561665+Jamesrkiv@users.noreply.github.com>
2025-05-09 11:30:39 -0400

    Fixed error in pharmacyRuleController.js

--- server/controllers/pharmRuleController.js
+++ server/controllers/pharmRuleController.js
@@ -22,8 +22,8 @@ const newPharmRule = async (req, res) => {
 	const { pharmacy_id, rule_ids } = req.body;
 	try {
 		// Construct values in format ($1, $2), ($1, $3), etc...
-		const values = rules_ids.map((_, i) => `($1, $${i + 2})`).join(', ');
-		const ids = [pharmacy_id, ...rules_ids];
+		const values = rule_ids.map((_, i) => `($1, $${i + 2})`).join(', ');
+		const ids = [pharmacy_id, ...rule_ids];
 		const result = await db.query(
 			`INSERT INTO pharmacy_rules (pharmacy_id, rules_id)
 				VALUES ${values}
