70992607bf3c3b95ae7dc0b584b55a5854c8d3fe
unknown <james@helmshomecare.com>
2025-07-16 12:05:16 -0400

    Updated db schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -5,8 +5,6 @@
 -- Dumped from database version 17.5
 -- Dumped by pg_dump version 17.5
 
--- Started on 2025-07-16 09:58:45
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -24,7 +22,6 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 217 (class 1259 OID 16389)
 -- Name: contacts; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -47,7 +44,6 @@ CREATE TABLE public.contacts (
 ALTER TABLE public.contacts OWNER TO postgres;
 
 --
--- TOC entry 218 (class 1259 OID 16397)
 -- Name: contacts_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -63,8 +59,6 @@ CREATE SEQUENCE public.contacts_id_seq
 ALTER SEQUENCE public.contacts_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4889 (class 0 OID 0)
--- Dependencies: 218
 -- Name: contacts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -72,7 +66,46 @@ ALTER SEQUENCE public.contacts_id_seq OWNED BY public.contacts.id;
 
 
 --
--- TOC entry 219 (class 1259 OID 16398)
+-- Name: logs; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.logs (
+    id integer NOT NULL,
+    level text NOT NULL,
+    acting_user text NOT NULL,
+    action text NOT NULL,
+    target_id text,
+    target_user text,
+    origin_ip text,
+    "timestamp" timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE public.logs OWNER TO postgres;
+
+--
+-- Name: logs_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.logs_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.logs_id_seq OWNER TO postgres;
+
+--
+-- Name: logs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.logs_id_seq OWNED BY public.logs.id;
+
+
+--
 -- Name: pharmacies; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -91,7 +124,6 @@ CREATE TABLE public.pharmacies (
 ALTER TABLE public.pharmacies OWNER TO postgres;
 
 --
--- TOC entry 220 (class 1259 OID 16405)
 -- Name: pharmacies_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -107,8 +139,6 @@ CREATE SEQUENCE public.pharmacies_id_seq
 ALTER SEQUENCE public.pharmacies_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4890 (class 0 OID 0)
--- Dependencies: 220
 -- Name: pharmacies_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -116,7 +146,6 @@ ALTER SEQUENCE public.pharmacies_id_seq OWNED BY public.pharmacies.id;
 
 
 --
--- TOC entry 221 (class 1259 OID 16406)
 -- Name: pharmacy_blurbs; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -129,7 +158,6 @@ CREATE TABLE public.pharmacy_blurbs (
 ALTER TABLE public.pharmacy_blurbs OWNER TO postgres;
 
 --
--- TOC entry 222 (class 1259 OID 16409)
 -- Name: pharmacy_blurbs_blurb_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -145,8 +173,6 @@ CREATE SEQUENCE public.pharmacy_blurbs_blurb_id_seq
 ALTER SEQUENCE public.pharmacy_blurbs_blurb_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4891 (class 0 OID 0)
--- Dependencies: 222
 -- Name: pharmacy_blurbs_blurb_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -154,7 +180,6 @@ ALTER SEQUENCE public.pharmacy_blurbs_blurb_id_seq OWNED BY public.pharmacy_blur
 
 
 --
--- TOC entry 223 (class 1259 OID 16410)
 -- Name: pharmacy_blurbs_pharmacy_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -170,8 +195,6 @@ CREATE SEQUENCE public.pharmacy_blurbs_pharmacy_id_seq
 ALTER SEQUENCE public.pharmacy_blurbs_pharmacy_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4892 (class 0 OID 0)
--- Dependencies: 223
 -- Name: pharmacy_blurbs_pharmacy_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -179,7 +202,6 @@ ALTER SEQUENCE public.pharmacy_blurbs_pharmacy_id_seq OWNED BY public.pharmacy_b
 
 
 --
--- TOC entry 224 (class 1259 OID 16411)
 -- Name: pharmacy_contacts; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -192,7 +214,6 @@ CREATE TABLE public.pharmacy_contacts (
 ALTER TABLE public.pharmacy_contacts OWNER TO postgres;
 
 --
--- TOC entry 225 (class 1259 OID 16414)
 -- Name: pharmacy_contacts_contact_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -208,8 +229,6 @@ CREATE SEQUENCE public.pharmacy_contacts_contact_id_seq
 ALTER SEQUENCE public.pharmacy_contacts_contact_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4893 (class 0 OID 0)
--- Dependencies: 225
 -- Name: pharmacy_contacts_contact_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -217,7 +236,6 @@ ALTER SEQUENCE public.pharmacy_contacts_contact_id_seq OWNED BY public.pharmacy_
 
 
 --
--- TOC entry 226 (class 1259 OID 16415)
 -- Name: pharmacy_contacts_pharmacy_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -233,8 +251,6 @@ CREATE SEQUENCE public.pharmacy_contacts_pharmacy_id_seq
 ALTER SEQUENCE public.pharmacy_contacts_pharmacy_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4894 (class 0 OID 0)
--- Dependencies: 226
 -- Name: pharmacy_contacts_pharmacy_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -242,7 +258,6 @@ ALTER SEQUENCE public.pharmacy_contacts_pharmacy_id_seq OWNED BY public.pharmacy
 
 
 --
--- TOC entry 227 (class 1259 OID 16416)
 -- Name: pharmacy_rules; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -255,7 +270,6 @@ CREATE TABLE public.pharmacy_rules (
 ALTER TABLE public.pharmacy_rules OWNER TO postgres;
 
 --
--- TOC entry 228 (class 1259 OID 16419)
 -- Name: pharmacy_rules_pharmacy_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -271,8 +285,6 @@ CREATE SEQUENCE public.pharmacy_rules_pharmacy_id_seq
 ALTER SEQUENCE public.pharmacy_rules_pharmacy_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4895 (class 0 OID 0)
--- Dependencies: 228
 -- Name: pharmacy_rules_pharmacy_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -280,7 +292,6 @@ ALTER SEQUENCE public.pharmacy_rules_pharmacy_id_seq OWNED BY public.pharmacy_ru
 
 
 --
--- TOC entry 229 (class 1259 OID 16420)
 -- Name: pharmacy_rules_rules_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -296,8 +307,6 @@ CREATE SEQUENCE public.pharmacy_rules_rules_id_seq
 ALTER SEQUENCE public.pharmacy_rules_rules_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4896 (class 0 OID 0)
--- Dependencies: 229
 -- Name: pharmacy_rules_rules_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -305,7 +314,6 @@ ALTER SEQUENCE public.pharmacy_rules_rules_id_seq OWNED BY public.pharmacy_rules
 
 
 --
--- TOC entry 230 (class 1259 OID 16421)
 -- Name: pharmacy_training; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -318,7 +326,6 @@ CREATE TABLE public.pharmacy_training (
 ALTER TABLE public.pharmacy_training OWNER TO postgres;
 
 --
--- TOC entry 231 (class 1259 OID 16424)
 -- Name: pharmacy_training_pharmacy_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -334,8 +341,6 @@ CREATE SEQUENCE public.pharmacy_training_pharmacy_id_seq
 ALTER SEQUENCE public.pharmacy_training_pharmacy_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4897 (class 0 OID 0)
--- Dependencies: 231
 -- Name: pharmacy_training_pharmacy_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -343,7 +348,6 @@ ALTER SEQUENCE public.pharmacy_training_pharmacy_id_seq OWNED BY public.pharmacy
 
 
 --
--- TOC entry 232 (class 1259 OID 16425)
 -- Name: pharmacy_training_training_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -359,8 +363,6 @@ CREATE SEQUENCE public.pharmacy_training_training_id_seq
 ALTER SEQUENCE public.pharmacy_training_training_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4898 (class 0 OID 0)
--- Dependencies: 232
 -- Name: pharmacy_training_training_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -368,7 +370,6 @@ ALTER SEQUENCE public.pharmacy_training_training_id_seq OWNED BY public.pharmacy
 
 
 --
--- TOC entry 233 (class 1259 OID 16426)
 -- Name: rules; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -381,7 +382,6 @@ CREATE TABLE public.rules (
 ALTER TABLE public.rules OWNER TO postgres;
 
 --
--- TOC entry 234 (class 1259 OID 16431)
 -- Name: rules_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -397,8 +397,6 @@ CREATE SEQUENCE public.rules_id_seq
 ALTER SEQUENCE public.rules_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4899 (class 0 OID 0)
--- Dependencies: 234
 -- Name: rules_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -406,7 +404,6 @@ ALTER SEQUENCE public.rules_id_seq OWNED BY public.rules.id;
 
 
 --
--- TOC entry 235 (class 1259 OID 16432)
 -- Name: training; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -420,7 +417,6 @@ CREATE TABLE public.training (
 ALTER TABLE public.training OWNER TO postgres;
 
 --
--- TOC entry 236 (class 1259 OID 16437)
 -- Name: training_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -436,8 +432,6 @@ CREATE SEQUENCE public.training_id_seq
 ALTER SEQUENCE public.training_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4900 (class 0 OID 0)
--- Dependencies: 236
 -- Name: training_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -445,7 +439,6 @@ ALTER SEQUENCE public.training_id_seq OWNED BY public.training.id;
 
 
 --
--- TOC entry 237 (class 1259 OID 16438)
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -461,7 +454,6 @@ CREATE TABLE public.users (
 ALTER TABLE public.users OWNER TO postgres;
 
 --
--- TOC entry 238 (class 1259 OID 16444)
 -- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -477,8 +469,6 @@ CREATE SEQUENCE public.users_id_seq
 ALTER SEQUENCE public.users_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4901 (class 0 OID 0)
--- Dependencies: 238
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -486,7 +476,6 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
--- TOC entry 239 (class 1259 OID 16445)
 -- Name: vn_blurbs; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -501,7 +490,6 @@ CREATE TABLE public.vn_blurbs (
 ALTER TABLE public.vn_blurbs OWNER TO postgres;
 
 --
--- TOC entry 240 (class 1259 OID 16450)
 -- Name: vn_blurbs_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -517,8 +505,6 @@ CREATE SEQUENCE public.vn_blurbs_id_seq
 ALTER SEQUENCE public.vn_blurbs_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4902 (class 0 OID 0)
--- Dependencies: 240
 -- Name: vn_blurbs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -526,7 +512,6 @@ ALTER SEQUENCE public.vn_blurbs_id_seq OWNED BY public.vn_blurbs.id;
 
 
 --
--- TOC entry 4690 (class 2604 OID 16451)
 -- Name: contacts id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -534,7 +519,13 @@ ALTER TABLE ONLY public.contacts ALTER COLUMN id SET DEFAULT nextval('public.con
 
 
 --
--- TOC entry 4694 (class 2604 OID 16452)
+-- Name: logs id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.logs ALTER COLUMN id SET DEFAULT nextval('public.logs_id_seq'::regclass);
+
+
+--
 -- Name: pharmacies id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -542,7 +533,6 @@ ALTER TABLE ONLY public.pharmacies ALTER COLUMN id SET DEFAULT nextval('public.p
 
 
 --
--- TOC entry 4697 (class 2604 OID 16453)
 -- Name: pharmacy_blurbs blurb_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -550,7 +540,6 @@ ALTER TABLE ONLY public.pharmacy_blurbs ALTER COLUMN blurb_id SET DEFAULT nextva
 
 
 --
--- TOC entry 4698 (class 2604 OID 16454)
 -- Name: pharmacy_blurbs pharmacy_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -558,7 +547,6 @@ ALTER TABLE ONLY public.pharmacy_blurbs ALTER COLUMN pharmacy_id SET DEFAULT nex
 
 
 --
--- TOC entry 4699 (class 2604 OID 16455)
 -- Name: pharmacy_contacts pharmacy_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -566,7 +554,6 @@ ALTER TABLE ONLY public.pharmacy_contacts ALTER COLUMN pharmacy_id SET DEFAULT n
 
 
 --
--- TOC entry 4700 (class 2604 OID 16456)
 -- Name: pharmacy_contacts contact_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -574,7 +561,6 @@ ALTER TABLE ONLY public.pharmacy_contacts ALTER COLUMN contact_id SET DEFAULT ne
 
 
 --
--- TOC entry 4701 (class 2604 OID 16457)
 -- Name: pharmacy_rules pharmacy_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -582,7 +568,6 @@ ALTER TABLE ONLY public.pharmacy_rules ALTER COLUMN pharmacy_id SET DEFAULT next
 
 
 --
--- TOC entry 4702 (class 2604 OID 16458)
 -- Name: pharmacy_rules rules_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -590,7 +575,6 @@ ALTER TABLE ONLY public.pharmacy_rules ALTER COLUMN rules_id SET DEFAULT nextval
 
 
 --
--- TOC entry 4703 (class 2604 OID 16459)
 -- Name: pharmacy_training pharmacy_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -598,7 +582,6 @@ ALTER TABLE ONLY public.pharmacy_training ALTER COLUMN pharmacy_id SET DEFAULT n
 
 
 --
--- TOC entry 4704 (class 2604 OID 16460)
 -- Name: pharmacy_training training_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -606,7 +589,6 @@ ALTER TABLE ONLY public.pharmacy_training ALTER COLUMN training_id SET DEFAULT n
 
 
 --
--- TOC entry 4705 (class 2604 OID 16461)
 -- Name: rules id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -614,7 +596,6 @@ ALTER TABLE ONLY public.rules ALTER COLUMN id SET DEFAULT nextval('public.rules_
 
 
 --
--- TOC entry 4706 (class 2604 OID 16462)
 -- Name: training id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -622,7 +603,6 @@ ALTER TABLE ONLY public.training ALTER COLUMN id SET DEFAULT nextval('public.tra
 
 
 --
--- TOC entry 4707 (class 2604 OID 16463)
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -630,7 +610,6 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
--- TOC entry 4709 (class 2604 OID 16464)
 -- Name: vn_blurbs id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -638,7 +617,6 @@ ALTER TABLE ONLY public.vn_blurbs ALTER COLUMN id SET DEFAULT nextval('public.vn
 
 
 --
--- TOC entry 4711 (class 2606 OID 16466)
 -- Name: contacts contacts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -647,7 +625,14 @@ ALTER TABLE ONLY public.contacts
 
 
 --
--- TOC entry 4713 (class 2606 OID 16468)
+-- Name: logs logs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.logs
+    ADD CONSTRAINT logs_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: pharmacies pharmacies_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -656,7 +641,6 @@ ALTER TABLE ONLY public.pharmacies
 
 
 --
--- TOC entry 4715 (class 2606 OID 16470)
 -- Name: pharmacy_blurbs pharmacy_blurbs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -665,7 +649,6 @@ ALTER TABLE ONLY public.pharmacy_blurbs
 
 
 --
--- TOC entry 4717 (class 2606 OID 16472)
 -- Name: pharmacy_contacts pharmacy_contacts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -674,7 +657,6 @@ ALTER TABLE ONLY public.pharmacy_contacts
 
 
 --
--- TOC entry 4719 (class 2606 OID 16474)
 -- Name: pharmacy_rules pharmacy_rules_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -683,7 +665,6 @@ ALTER TABLE ONLY public.pharmacy_rules
 
 
 --
--- TOC entry 4721 (class 2606 OID 16476)
 -- Name: pharmacy_training pharmacy_training_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -692,7 +673,6 @@ ALTER TABLE ONLY public.pharmacy_training
 
 
 --
--- TOC entry 4723 (class 2606 OID 16478)
 -- Name: rules rules_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -701,7 +681,6 @@ ALTER TABLE ONLY public.rules
 
 
 --
--- TOC entry 4725 (class 2606 OID 16480)
 -- Name: training training_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -710,7 +689,6 @@ ALTER TABLE ONLY public.training
 
 
 --
--- TOC entry 4728 (class 2606 OID 16482)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -719,7 +697,6 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 4730 (class 2606 OID 16486)
 -- Name: vn_blurbs vn_blurbs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -728,7 +705,6 @@ ALTER TABLE ONLY public.vn_blurbs
 
 
 --
--- TOC entry 4726 (class 1259 OID 16528)
 -- Name: unique_lower_username; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -736,7 +712,6 @@ CREATE UNIQUE INDEX unique_lower_username ON public.users USING btree (lower(use
 
 
 --
--- TOC entry 4731 (class 2606 OID 16487)
 -- Name: pharmacy_blurbs fk_blurb; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -745,7 +720,6 @@ ALTER TABLE ONLY public.pharmacy_blurbs
 
 
 --
--- TOC entry 4732 (class 2606 OID 16492)
 -- Name: pharmacy_blurbs fk_pharm; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -754,7 +728,6 @@ ALTER TABLE ONLY public.pharmacy_blurbs
 
 
 --
--- TOC entry 4733 (class 2606 OID 16497)
 -- Name: pharmacy_contacts pharmacy_contacts_contact_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -763,7 +736,6 @@ ALTER TABLE ONLY public.pharmacy_contacts
 
 
 --
--- TOC entry 4734 (class 2606 OID 16502)
 -- Name: pharmacy_contacts pharmacy_contacts_pharmacy_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -772,7 +744,6 @@ ALTER TABLE ONLY public.pharmacy_contacts
 
 
 --
--- TOC entry 4735 (class 2606 OID 16507)
 -- Name: pharmacy_rules pharmacy_rules_pharmacy_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -781,7 +752,6 @@ ALTER TABLE ONLY public.pharmacy_rules
 
 
 --
--- TOC entry 4736 (class 2606 OID 16512)
 -- Name: pharmacy_rules pharmacy_rules_rules_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -790,7 +760,6 @@ ALTER TABLE ONLY public.pharmacy_rules
 
 
 --
--- TOC entry 4737 (class 2606 OID 16517)
 -- Name: pharmacy_training pharmacy_training_pharmacy_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -799,15 +768,12 @@ ALTER TABLE ONLY public.pharmacy_training
 
 
 --
--- TOC entry 4738 (class 2606 OID 16522)
 -- Name: pharmacy_training pharmacy_training_training_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.pharmacy_training
     ADD CONSTRAINT pharmacy_training_training_id_fkey FOREIGN KEY (training_id) REFERENCES public.training(id);
 
-
--- Completed on 2025-07-16 09:58:46
 
 --
 -- PostgreSQL database dump complete
