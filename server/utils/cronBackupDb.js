f43c61d0042b7976e84b3e648a82cd253411fff5
unknown <james@helmshomecare.com>
2025-07-24 08:40:51 -0400

    Added system log for db backups

--- server/utils/cronBackupDb.js
+++ server/utils/cronBackupDb.js
@@ -1,11 +1,17 @@
 const cron = require('node-cron');
 const backupAndUpload = require('../db/dbBackup');
+const logger = require('./logger');
 
 cron.schedule('0 8 * * 5', async () => {
 	console.log('[Cron] Starting scheduled DB backup...');
 	backupAndUpload()
 	.then(filename => {
 		console.log(`[Cron] Uploaded ${filename} to SharePoint`);
+		// Logging
+		logger.info({
+			actingUser: 'SYSTEM',
+			action: 'Created database backup',
+		});
 	})
 	.catch(err => {
 		console.error('[Cron] Backup failed:', err.message || err);
